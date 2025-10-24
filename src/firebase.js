3dd128e2623883b725cfddfba42923b02e5d2856
ejaymedia <eliafoeri@gmail.com>
2025-10-24 18:49:43 +0100

    backend

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,22 @@
+// firebase.js
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBh_n_4YLe6w26_CbRR-1toZfv0nD3GlTU",
-  authDomain: "church-website-25.firebaseapp.com",
-  projectId: "church-website-25",
-  storageBucket: "church-website-25.firebasestorage.app",
-  messagingSenderId: "18049781291",
-  appId: "1:18049781291:web:c93e8b2639b1fd9f0e246e"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore(app);
+// Export Firebase services
 export const auth = getAuth(app);
+export const db = getFirestore(app);
 export const storage = getStorage(app);
